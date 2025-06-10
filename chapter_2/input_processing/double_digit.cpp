b567d0c71d3e9553093ce4274c6ef26aacf1acb8
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2025-06-10 13:59:39 +0600

    double digits

--- chapter_2/input_processing/double_digit.cpp
+++ chapter_2/input_processing/double_digit.cpp
@@ -2,13 +2,14 @@
 using std::cin;
 using std::cout;
 
-int main() {
-    int digit;
-    cout << "Enter a single digit number, 0-9: ";
-    cin >> digit;
+int doubleDigitValue(int digit) {
     int doubledDigit = digit * 2;
     int sum;
     if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
     else sum = doubledDigit;
     return sum;
+}
+
+int main() {
+    doubleDigitValue();
 }
