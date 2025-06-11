c30d5e2b2fa18b0c49d528806e90b139e665e66f
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2025-06-11 15:00:28 +0600

    integer to character conversion

--- chapter_2/tracking_state/int_to_char_conversion.cpp
+++ chapter_2/tracking_state/int_to_char_conversion.cpp
@@ -11,10 +11,20 @@ int main() {
     // int overallNumber = digit1 * 10 + digit2;
     // cout << "That number as an integer: " << overallNumber << "\n";
 
-    cout << "Enter a two-digit number: ";
-    char digitChar = cin.get();
-    int overallNumber = (digitChar - '0') * 10;
-    digitChar = cin.get();
-    overallNumber += (digitChar - '0');
-    cout << "That number as an integer: " << overallNumber << "\n";  
+    // cout << "Enter a two-digit number: ";
+    // char digitChar = cin.get();
+    // int overallNumber = (digitChar - '0') * 10;
+    // digitChar = cin.get();
+    // overallNumber += (digitChar - '0');
+    // cout << "That number as an integer: " << overallNumber << "\n";
+
+    cout << "Enter a number with as many digits as you like: ";
+    char digtiChar = cin.get();
+    int number = (digtiChar - '0');
+    digtiChar = cin.get();
+    while (digtiChar != 10) {
+        number = number * 10 + (digtiChar - '0');
+        digtiChar = cin.get();
+    }
+    cout << "Number entered: " << number << "\n";
 }
